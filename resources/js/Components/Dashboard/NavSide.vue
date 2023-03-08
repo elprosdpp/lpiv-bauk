14974be95211e56274cbc7adc7a1e7a98bad4fd8
elprosdpp <sugengpratama45@gmail.com>
2023-03-08 12:12:35 +0700

    add sidebar mobile responsive & dark mode

--- resources/js/Components/Dashboard/NavSide.vue
+++ resources/js/Components/Dashboard/NavSide.vue
@@ -1,5 +1,7 @@
 <script setup>
 import {ref} from 'vue';
+import { useDark, useToggle } from '@vueuse/core';
+import {Head, Link, useForm, usePage, router} from '@inertiajs/vue3';
 import {
     Disclosure,
     DisclosureButton,
@@ -11,25 +13,149 @@ import {
     DialogTitle,
 } from '@headlessui/vue';
 
-import {Link} from '@inertiajs/vue3'
 
-const isOpen = ref(false)
+const isOpen = ref(false);
+const isProfile = ref(false);
+const sidebar = ref(false);
+
+const user = usePage().props.auth.user;
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
 
 function closeModal() {
-    isOpen.value = false
+    isOpen.value = false;
 }
 
 function openModal() {
-    isOpen.value = true
+    isOpen.value = true;
 }
 
 </script>
 
 <template>
+    <nav class="fixed z-30 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <div class="px-3 py-3 lg:px-5 lg:pl-3">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center justify-start">
+                    <button @click="sidebar = !sidebar" id="toggleSidebarMobile" aria-controls="sidebar" aria-expanded="true"
+                            class="p-2 text-gray-600 rounded cursor-pointer lg:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <svg v-if="!sidebar" id="toggleSidebarMobileHamburger" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path clip-rule="evenodd"
+                                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                  fill-rule="evenodd"></path>
+                        </svg>
+                        <svg v-if="sidebar" id="toggleSidebarMobileClose" class="w-6 h-6" fill="currentColor"
+                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path clip-rule="evenodd"
+                                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                  fill-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                    <a class="flex ml-2 md:mr-24" href="#">
+                        <img alt="FlowBite Logo" class="h-8 mr-3"
+                             src="https://flowbite-admin-dashboard.vercel.app/images/logo.svg">
+                        <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span>
+                    </a>
+                    <form action="#" class="hidden lg:block lg:pl-3.5" method="GET">
+                        <label class="sr-only" for="topbar-search">Search</label>
+                        <div class="relative mt-1 lg:w-96">
+                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+                                     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path clip-rule="evenodd"
+                                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                          fill-rule="evenodd"></path>
+                                </svg>
+                            </div>
+                            <input id="topbar-search"
+                                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                   name="email"
+                                   placeholder="Search"
+                                   type="text">
+                        </div>
+                    </form>
+                </div>
+                <div class="flex items-center">
+                    <button id="theme-toggle"
+                            @click="toggleDark()"
+                            class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+                            data-tooltip-target="tooltip-toggle"
+                            type="button">
+                        <svg
+                            v-if="!isDark"
+                            class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                        </svg>
+                        <svg v-if="isDark"
+                             class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                clip-rule="evenodd"
+                                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                fill-rule="evenodd"></path>
+                        </svg>
+                    </button>
+
+                    <div class="flex items-center ml-3">
+                        <div>
+                            <button
+                                id="user-menu-button-2"
+                                aria-expanded="false"
+                                class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                                data-dropdown-toggle="dropdown-2"
+                                type="button" @click="isProfile = !isProfile">
+                                <span class="sr-only">Open user menu</span>
+                                <img :src="`/storage/`+ user.image_profile"
+                                     alt="user photo"
+                                     class="w-8 h-8 rounded-full"/>
+                            </button>
+                        </div>
+
+                        <div
+                            v-show="isProfile"
+                            class="z-50  my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
+                            data-popper-placement="bottom"
+                            style="position: absolute; top: 3rem; right: 15px">
+                            <div class="px-4 py-3" role="none">
+                                <p class="text-sm text-gray-900 dark:text-white" role="none">
+                                    {{ user.name }}
+                                </p>
+                                <p class="text-xs font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                                    {{ user.email }}
+                                </p>
+                            </div>
+                            <ul class="py-1" role="none" >
+                                <li @click="isProfile = !isProfile">
+                                    <a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                       href="#"
+                                       role="menuitem">Profile</a>
+                                </li>
+                                <li @click="isProfile = !isProfile">
+                                    <a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                       href="#"
+                                       role="menuitem">Settings</a>
+                                </li>
+                                <li @click="isProfile = !isProfile">
+                                    <a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                       href="#"
+                                       role="menuitem">Logout</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div id="sidebarBackdrop" :class="sidebar ? 'fixed inset-0 z-10 bg-gray-500/50 dark:bg-gray-900/50' : 'fixed hidden inset-0 z-10 bg-gray-500/50 dark:bg-gray-900/50'"></div>
+
     <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
         <aside id="sidebar"
                aria-label="Sidebar"
-               class="fixed top-0 left-0 z-20 flex flex-col flex-shrink-0 hidden w-64 h-full pt-16 font-normal duration-75 lg:flex transition-width">
+               :class="sidebar ? 'fixed top-0 left-0 z-20 flex flex-col flex-shrink-0 w-64 h-full pt-16 font-normal duration-75 lg:flex transition-width' : 'fixed top-0 left-0 z-20 flex flex-col hidden flex-shrink-0 w-64 h-full pt-16 font-normal duration-75 lg:flex transition-width'">
             <div
                 class="relative flex flex-col flex-1 min-h-0 pt-0 bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex flex-col flex-1 pt-5 pb-4 overflow-y-auto">
@@ -76,7 +202,8 @@ function openModal() {
                                         class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">
                                         <svg
                                             aria-hidden="true"
-                                            class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20"
+                                            class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                                            fill="currentColor" viewBox="0 0 20 20"
                                             xmlns="http://www.w3.org/2000/svg">
                                             <path
                                                 d="M10 9a3 3 0 100-6 3 3 0 000 6zM6 8a2 2 0 11-4 0 2 2 0 014 0zM1.49 15.326a.78.78 0 01-.358-.442 3 3 0 014.308-3.516 6.484 6.484 0 00-1.905 3.959c-.023.222-.014.442.025.654a4.97 4.97 0 01-2.07-.655zM16.44 15.98a4.97 4.97 0 002.07-.654.78.78 0 00.357-.442 3 3 0 00-4.308-3.517 6.484 6.484 0 011.907 3.96 2.32 2.32 0 01-.026.654zM18 8a2 2 0 11-4 0 2 2 0 014 0zM5.304 16.19a.844.844 0 01-.277-.71 5 5 0 019.947 0 .843.843 0 01-.277.71A6.975 6.975 0 0110 18a6.974 6.974 0 01-4.696-1.81z"></path>
@@ -98,11 +225,11 @@ function openModal() {
                                             </li>
                                             <li>
                                                 <a class="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700 "
-                                                   href="https://flowbite-admin-dashboard.vercel.app/playground/sidebar/">Permission</a>
+                                                   href="https://flowbite-admin-dashboard.vercel.app/playground/sidebar/NavSide.vue">Permission</a>
                                             </li>
                                             <li>
                                                 <a class="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700 "
-                                                   href="https://flowbite-admin-dashboard.vercel.app/playground/sidebar/">User</a>
+                                                   href="https://flowbite-admin-dashboard.vercel.app/playground/sidebar/NavSide.vue">User</a>
                                             </li>
                                         </ul>
                                     </DisclosurePanel>
@@ -179,9 +306,9 @@ function openModal() {
                                                 </div>
 
                                                 <div class="mt-4">
-                                                    <Link :href="route('logout')"
-                                                          class="inline-flex mr-2 justify-center rounded-md border border-transparent bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                                                          method="post">
+                                                    <Link :href="route('logout')" as="button" class="inline-flex mr-2 justify-center rounded-md border border-transparent bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                                          method="post"
+                                                    >
                                                         Logout
                                                     </Link>
                                                     <button
--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -1,21 +1,17 @@
 <script setup>
 import Navbar from "../Components/Dashboard/Navbar.vue";
-import Sidebar from "../Components/Dashboard/Sidebar.vue";
+import NavSide from "../Components/Dashboard/NavSide.vue";
 
 </script>
 
 <template>
-    <!--Navbar-->
-    <Navbar/>
-
-    <!--Sidebar-->
-    <Sidebar/>
+    <!--Navbar & Sidebar-->
+    <NavSide/>
 
     <!-- Content -->
-    <div id="sidebarBackdrop" class="fixed inset-0 z-10 hidden dark:bg-gray-900/90"></div>
-    <div id="main-content" class="relative h-full overflow-y-auto lg:ml-64 dark:bg-gray-900">
+    <div id="main-content" class="relative h-[100vh] overflow-y-auto lg:ml-64 dark:bg-gray-900">
         <main>
-            <div class="p-4">
+            <div class="p-4 dark:border-gray-700">
                 <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
                     <slot/>
                 </div>
