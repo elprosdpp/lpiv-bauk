032c93ea671e374311814c70a48f2fce70ab0224
elprosdpp <sugengpratama45@gmail.com>
2023-03-10 09:56:57 +0700

    toast notification fix position

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -1,5 +1,4 @@
 <script setup>
-import Navbar from "../Components/Dashboard/Navbar.vue";
 import NavSide from "../Components/Dashboard/NavSide.vue";
 import Toast from '@/Components/Toast.vue';
 
@@ -9,14 +8,14 @@ import Toast from '@/Components/Toast.vue';
     <!--Navbar & Sidebar-->
     <NavSide/>
 
+    <!--Toast-->
+    <div v-if="$page.props.flash.message" class="fixed right-10 top-[6rem] z-[999]">
+        <Toast :message="$page.props.flash.message"/>
+    </div>
+
     <!-- Content -->
     <div id="main-content" class="relative overflow-y-auto lg:ml-64 dark:bg-gray-900">
         <main>
-            <!--Toast-->
-            <div v-if="$page.props.flash.message" class="absolute right-10 top-10 z-10">
-                <Toast :message="$page.props.flash.message"/>
-            </div>
-
             <!--Content here-->
             <div class="p-4 dark:border-gray-700">
                 <div class="p-4">
